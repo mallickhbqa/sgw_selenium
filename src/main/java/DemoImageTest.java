0aeb04cc81f39b4ca054f3fc3f9a06d62323245d
mallickhbqa <himandribhusanmallick@gmail.com>
2016-05-30 18:29:59 +0530

    SWG_In_Selenium

---
+++ src/main/java/DemoImageTest.java
@@ -0,0 +1,21 @@
+import java.io.File;
+
+//import com.asprise.ocr.Ocr;
+
+public class DemoImageTest {
+
+	/*public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       
+		Ocr.setUp(); // one time setup
+		Ocr ocr = new Ocr(); // create a new OCR engine
+		ocr.startEngine("eng", Ocr.SPEED_FASTEST); // English
+		String s = ocr.recognize(new File[] {new File("C:\\Users\\mallickh\\Desktop\\newport_screenshot - Copy.png")}, Ocr.RECOGNIZE_TYPE_ALL, Ocr.OUTPUT_FORMAT_PLAINTEXT);
+		System.out.println("Result: " + s);
+		// ocr more images here ...
+		ocr.stopEngine();
+		
+		
+	}*/
+
+}
---
+++ src/main/java/DemoImageTest2.java
@@ -0,0 +1,36 @@
+import java.io.File;
+
+//import com.asprise.ocr.Ocr;
+
+public class DemoImageTest2 {
+
+	/*public static  boolean compareImageTxt(String path1,String Path2) {
+		// TODO Auto-generated method stub
+
+		
+		Ocr.setUp(); // one time setup
+		Ocr ocr = new Ocr(); // create a new OCR engine
+		ocr.startEngine("eng", Ocr.SPEED_FASTEST); // English
+		String s = ocr.recognize(new File[] {new File("path1")}, Ocr.RECOGNIZE_TYPE_ALL, Ocr.OUTPUT_FORMAT_PLAINTEXT);
+		System.out.println("Result: " + s);
+		// ocr more images here ...
+		String s2=ocr.recognize(new File[] {new File(Path2)}, Ocr.RECOGNIZE_TYPE_ALL, Ocr.OUTPUT_FORMAT_PLAINTEXT);
+		System.out.println("Result: " + s2);
+		if(s.equals(s2))
+		{
+			return true;
+		}
+		else
+		{
+			
+			return false;
+		}
+		
+		
+		
+		
+		
+		
+	}*/
+
+}
